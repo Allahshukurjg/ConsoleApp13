faa5eb5c73d2e11df223aaa18914e7cf38ae84ca
HP <HP@DESKTOP-DITLK6U>
2022-02-16 09:06:15 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Salam qadan alim netersen" };
+
+            foreach (string join in array)
+            {
+                foreach (char text in join)
+
+                    if (char.IsUpper(text))
+
+                    {
+                        Console.Write(char.ToLower(text));  
+                    }
+                    else
+                    {
+                        Console.Write(char.ToUpper(text));
+                    }
+                  
+             }
+        }
+    }
+}
